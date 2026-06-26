46c049cfa3eea3c7eeb202ddabc884c862769222
Chloe Prime <chloe_von_einzbern@qq.com>
2026-06-27 02:01:31 +0800

    Port to TaCZ 1.1.8 + SSR V5
    (cherry picked from commit 94989aecd641e20e4445b55fb605da935a4ecb92)

--- src/main/java/mod/chloeprime/thirdpersonshooting/mixin/client/MixinTacAimingHandler.java
+++ src/main/java/mod/chloeprime/thirdpersonshooting/mixin/client/MixinTacAimingHandler.java
@@ -1,6 +1,9 @@
 package mod.chloeprime.thirdpersonshooting.mixin.client;
 
-import com.github.exopandora.shouldersurfing.api.client.ShoulderSurfing;
+import com.github.exopandora.shouldersurfing.api.client.IShoulderSurfing;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.tacz.guns.api.item.IGun;
 import com.tacz.guns.client.event.CameraSetupEvent;
 import com.tacz.guns.client.gameplay.LocalPlayerAim;
@@ -9,38 +12,35 @@ import net.minecraft.world.item.ItemStack;
 import net.neoforged.fml.util.thread.EffectiveSide;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static mod.chloeprime.thirdpersonshooting.client.ClientConfig.CONSTANT_AIMING_ZOOM_SCALE;
 import static mod.chloeprime.thirdpersonshooting.client.ClientConfig.CONSTANT_AIMING_ZOOM_SCALE_VALUE;
 
 @Mixin(value = LocalPlayerAim.class, remap = false)
 public class MixinTacAimingHandler {
-    @SuppressWarnings("InvalidInjectorMethodSignature")
-    @Inject(method = "getAlphaProgress", at = @At("HEAD"), cancellable = true, require = 0)
-    private void lockSpeedAtSsMode_103(GunData gunData, CallbackInfoReturnable<Float> cir) {
+    @ModifyReturnValue(method = "getAlphaProgress", at = @At("RETURN"), require = 0)
+    private float lockSpeedAtSsMode_103(float original, GunData gunData) {
         if (EffectiveSide.get().isClient() &&
                 CONSTANT_AIMING_ZOOM_SCALE.get() &&
-                ShoulderSurfing.getInstance().isShoulderSurfing()
+                IShoulderSurfing.getInstance().isShoulderSurfing()
         ) {
-            cir.setReturnValue(1F);
+            return 1;
         }
+        return original;
     }
 
     @Mixin(value = CameraSetupEvent.class, remap = false)
     public static class MixinFovEvent {
-        @Redirect(
-                method = {"applyScopeMagnification"},
+        @WrapOperation(
+                method = "applyScopeMagnification",
                 at = @At(
                         value = "INVOKE",
                         target = "Lcom/tacz/guns/api/item/IGun;getAimingZoom(Lnet/minecraft/world/item/ItemStack;)F"))
-        private static float redirectFov(IGun gun, ItemStack stack) {
-            if (CONSTANT_AIMING_ZOOM_SCALE.get() && ShoulderSurfing.getInstance().isShoulderSurfing()) {
+        private static float redirectFov(IGun gun, ItemStack stack, Operation<Float> original) {
+            if (CONSTANT_AIMING_ZOOM_SCALE.get() && IShoulderSurfing.getInstance().isShoulderSurfing()) {
                 return CONSTANT_AIMING_ZOOM_SCALE_VALUE.get().floatValue();
             }
-            return gun.getAimingZoom(stack);
+            return original.call(gun, stack);
         }
     }
 }
